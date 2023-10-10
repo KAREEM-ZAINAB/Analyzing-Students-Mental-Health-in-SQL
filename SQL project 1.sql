ac5e28adfbad5705fc781f28f6c119203426b07e
Zainab Kareem <72425020+KAREEM-ZAINAB@users.noreply.github.com>
2023-10-10 17:20:30 +0100

    Analzing-Students-Mental-health.sql

--- SQL project 1.sql
+++ SQL project 1.sql
@@ -1,38 +1,46 @@
--- Start coding here...
+---Explore and understand the data alias result with count_inter_dom
 SELECT COUNT(*) AS count_inter_dom
 FROM students.csv;
 
+---Group records to find out the number of international and domestic students alias result with total_records
 SELECT inter_dom, COUNT(*) AS total_records
 FROM students.csv
 GROUP BY inter_dom;
 
+---Filter by international students using the WHERE clause 
 SELECT *
 FROM students.csv
 WHERE inter_dom = 'Inter';
 
+---Filter by domestic students using WHERE clause 
 SELECT *
 FROM students.csv
 WHERE inter_dom = 'Dom';
 
+---Filter by the unknown status of students using the WHERE clause 
 SELECT *
 FROM  students.csv
 WHERE inter_dom IS NULL;
 
+---Query summary statistics for all students using the MIN, MAX and AVG functions
 SELECT MIN(todep) AS min_phq,
 	MAX(todep) AS max_phq,
 		ROUND(AVG(todep), 2) AS avg_phq
 FROM students.csv;
 
+---Query summary statistics for international students score of acculturative stress
 SELECT MIN(tosc) AS min_scs,
 	MAX(tosc) AS max_scs,
 		ROUND(AVG(tosc), 2) AS avg_scs
 FROM students.csv;
 
+---Query summary statistics for international students score of social connectedness
 SELECT MIN(toas) AS min_as,
 	MAX(toas) AS max_as,
 	ROUND(AVG(toas), 2) AS avg_asiss
 FROM students.csv;
 
+---Query summary statistics for international students score of depression
 SELECT MIN(todep) AS min_phq,
 	MAX(todep) AS max_phq,
 	ROUND(AVG(todep), 2) AS avg_phq,
@@ -45,6 +53,7 @@ SELECT MIN(todep) AS min_phq,
 FROM students.csv
 WHERE inter_dom = 'Inter';
 
+---Calculate the length of stay of international students and how it impacts their diagnostic tests
 SELECT stay AS length_of_stay, 
 ROUND(AVG(todep), 2) AS avg_phq,
 ROUND(AVG(tosc), 2) AS avg_scs,
